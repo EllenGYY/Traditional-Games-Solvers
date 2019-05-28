ef0e7b1a5f7894b9d0e7549cf006356304112eb8
yiyingu <yiyingu0512@gmail.com>
2019-05-28 15:07:59 +0800

    log.txt

--- script/24points.js
+++ script/24points.js
@@ -35,7 +35,7 @@ function plus_minus_multi_divide(array) {
     return sequence;
 }
 
-function analysis_back(x, m, n, initial, first_round, second_round, first_round_backup, second_round_backup,inistr) {
+function analysis_back(x, m, n, initial, first_round, second_round, first_round_backup, second_round_backup, inistr, target) {
     let numbers1 = new Array([0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]);
     let numbers2 = new Array([0, 1], [0, 2], [1, 2]);
     let numbers3 = new Array([0, 1]);
@@ -77,49 +77,51 @@ function analysis_back(x, m, n, initial, first_round, second_round, first_round_
         third_line =
             String(second_round[m][numbers3[z][0]])
             + symbols[y] + String(second_round[m][numbers3[z][1]])
-            + "=24";
+            + "="+ target;
     }
     else {
         third_line =
             String(second_round[m][numbers3[z][1]])
             + "/" + String(second_round[m][numbers3[z][0]])
-            + "=24";
+            + "=" + target;
     }
-    let complete_string = inistr + "算得出24点" + "\r\n" + first_line + "\r\n" + second_line + "\r\n" + third_line;
+    let complete_string = inistr + " can have a result of " + target + "." + "<br/>" + first_line + "<br/>" + second_line + "<br/>" + third_line;
     return complete_string;
 }
 
-alert("输入4个正整数以计算24点（按下方按钮继续）：");
-let bool;
-let initial = new Array();
-let input = prompt("第一个数字:");
-let inistr = "".concat(input);
-initial.push(Number(input));
-input = prompt("第二个数字:");
-inistr = inistr.concat(","+input);
-initial.push(Number(input));
-input = prompt("第三个数字:");
-inistr = inistr.concat(","+input);
-initial.push(Number(input));
-input = prompt("第四个数字:");
-inistr = inistr.concat(","+input);
-initial.push(Number(input));
-let first_round = plus_minus_multi_divide(initial);
-for (let x = 0; x < first_round.length; x++) {
-    [...first_round_backup] = first_round[x]
-    let second_round = plus_minus_multi_divide(first_round[x]);
-    for (let m = 0; m < second_round.length; m++) {
-        [...second_round_backup] = second_round[m]
-        let final = plus_minus_multi_divide(second_round[m]);
-        for (let n = 0; n < final.length; n++) {
-            if (final[n][0] == 24) {
-                alert(analysis_back(x, m, n, initial, first_round, second_round, first_round_backup, second_round_backup,inistr));
-                bool = true;
-                break;
+function calculate_result() {
+    let bool;
+    let initial = new Array();
+    let input = document.getElementById("first").value;
+    let inistr = "".concat(input);
+    initial.push(Number(input));
+    input = document.getElementById("second").value;
+    inistr = inistr.concat("," + input);
+    initial.push(Number(input));
+    input = document.getElementById("third").value;
+    inistr = inistr.concat("," + input);
+    initial.push(Number(input));
+    input = document.getElementById("fourth").value;
+    inistr = inistr.concat("," + input);
+    initial.push(Number(input));
+    target = document.getElementById("target").value;
+    let first_round = plus_minus_multi_divide(initial);
+    for (let x = 0; x < first_round.length; x++) {
+        [...first_round_backup] = first_round[x]
+        let second_round = plus_minus_multi_divide(first_round[x]);
+        for (let m = 0; m < second_round.length; m++) {
+            [...second_round_backup] = second_round[m]
+            let final = plus_minus_multi_divide(second_round[m]);
+            for (let n = 0; n < final.length; n++) {
+                if (final[n][0] == Number(target)) {
+                    document.getElementById("answer").innerHTML=analysis_back(x, m, n, initial, first_round, second_round, first_round_backup, second_round_backup, inistr,target);
+                    bool = true;
+                    break;
+                }
             }
+            if (bool) { break };
         }
         if (bool) { break };
     }
-    if (bool) { break };
+    if (!bool) { document.getElementById("answer").innerHTML=inistr + "can never have a result of " + target + "."};
 }
-if (!bool) { alert(inistr + "算不出24点") };
