01ebcc7b1df73a02a4ccc96cbe40655f648f3b11
yiyingu <yiyingu0512@gmail.com>
2019-06-02 16:19:18 +0800

    update 24points.js

--- script/24points.js
+++ script/24points.js
@@ -1,9 +1,4 @@
-function sortNumber(a, b) {
-    return b - a
-}
-
 function plus_minus_multi_divide(array) {
-    array = array.sort(sortNumber);
     let x = array.length;
     let sequence = new Array();
     for (let i = 0; i < x - 1; i++) {
@@ -14,7 +9,7 @@ function plus_minus_multi_divide(array) {
             let variable = array[i] + array[j];
             temp.push(variable);
             sequence.push([...temp]);
-            variable = array[i] - array[j];
+            variable = Math.abs(array[i] - array[j]);
             temp.pop();
             temp.push(variable);
             sequence.push([...temp]);
@@ -35,97 +30,96 @@ function plus_minus_multi_divide(array) {
     return sequence;
 }
 
-function analysis_back(x, m, n, initial, first_round, second_round, first_round_backup, second_round_backup, inistr, target) {
+function sub_analysis(x, y, reason, number) {
+    let symbols = new Array("+", "-", "*", "/");
+    let this_line;
+    if (x != 4 && x != 1) {
+        this_line =
+            "(" + String(reason[number[y][0]])
+            + symbols[x] + String(reason[number[y][1]]) + ")";
+    }
+    else if (x == 1) {
+        if (reason[number[y][0]] >= reason[number[y][1]]) {
+            this_line =
+                "(" + String(reason[number[y][0]])
+                + "-" + String(reason[number[y][1]]) + ")";
+        }
+        else {
+            this_line =
+                "(" + String(reason[number[y][1]])
+                + "-" + String(reason[number[y][0]]) + ")";
+        }
+    }
+    else {
+        this_line =
+            "(" + String(reason[number[y][1]])
+            + "/" + String(reason[number[y][0]]) + ")";
+    }
+    return this_line;
+
+}
+function analysis_back(k, m, n, initial, first_round, second_round, target) {
     let numbers1 = new Array([0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]);
     let numbers2 = new Array([0, 1], [0, 2], [1, 2]);
     let numbers3 = new Array([0, 1]);
-    let symbols = new Array("+", "-", "*", "/");
-    let y = x % 5;
-    let z = (x - y) / 5;
-    let first_line;
-    let second_line;
-    let third_line;
-    if (y != 4) {
-        first_line =
-            String(initial[numbers1[z][0]])
-            + symbols[y] + String(initial[numbers1[z][1]])
-            + "=" + String(first_round_backup[2]);
+    let not_inserted = false;
+    let x = k % 5;
+    let y = (k - x) / 5;
+    let first_line = sub_analysis(x,y,initial,numbers1);
+    x = m % 5;
+    y = (m - x) / 5;
+    let second_line = sub_analysis(x,y,first_round[k],numbers2);
+    if(y != 0){
+        second_line = second_line.replace(String(first_round[k][2]),first_line);
     }
-    else {
-        first_line =
-            String(initial[numbers1[z][1]])
-            + "/" + String(initial[numbers1[z][0]])
-            + "=" + String(first_round_backup[2]);
+    else{
+        not_inserted = true;
     }
-    y = m % 5;
-    z = (m - y) / 5;
-    if (y != 4) {
-        second_line =
-            String(first_round[x][numbers2[z][0]])
-            + symbols[y] + String(first_round[x].sort(sortNumber)[numbers2[z][1]])
-            + "=" + String(second_round_backup[1]);
+    x = n % 5;
+    y = (n - x) / 5;
+    let third_line = sub_analysis(x,y,second_round[m],numbers3);
+    if(not_inserted){
+        third_line = third_line.replace(String(first_round[k][2]),first_line);
+        third_line = third_line.replace(String(second_round[m][1]),second_line);
     }
-    else {
-        second_line =
-            String(first_round[x][numbers2[z][1]])
-            + "/" + String(first_round[x].sort(sortNumber)[numbers2[z][0]])
-            + "=" + String(second_round_backup[1]);
+    else{
+        third_line = third_line.replace(String(second_round[m][1]),second_line);
     }
-    y = n % 5;
-    z = (n - y) / 5;
-    if (y != 4) {
-        third_line =
-            String(second_round[m][numbers3[z][0]])
-            + symbols[y] + String(second_round[m][numbers3[z][1]])
-            + "=" + target;
-    }
-    else {
-        third_line =
-            String(second_round[m][numbers3[z][1]])
-            + "/" + String(second_round[m][numbers3[z][0]])
-            + "=" + target;
-    }
-    let complete_string = inistr + " can have a result of " + target + "." + "<br/>" + first_line + "<br/>" + second_line + "<br/>" + third_line + "<br/>" + "<br/>";
+    let complete_string = "<br/>" + third_line +"=" + target;
     return complete_string;
 }
 
 function calculate_result() {
     let count = 0;
     let bool_stop;
-    let result_all = "";
+    let result_all = new Array();
     let initial = new Array();
     let input = document.getElementById("first").value;
-    let inistr = "".concat(input);
     initial.push(Number(input));
     input = document.getElementById("second").value;
-    inistr = inistr.concat("," + input);
     initial.push(Number(input));
     input = document.getElementById("third").value;
-    inistr = inistr.concat("," + input);
     initial.push(Number(input));
     input = document.getElementById("fourth").value;
-    inistr = inistr.concat("," + input);
     initial.push(Number(input));
     target = document.getElementById("target").value;
     let oaArr = document.all('one_or_all');
     let first_round = plus_minus_multi_divide(initial);
-    for (let x = 0; x < first_round.length; x++) {
-        [...first_round_backup] = first_round[x]
-        let second_round = plus_minus_multi_divide(first_round[x]);
+    for (let k = 0; k < first_round.length; k++) {
+        let second_round = plus_minus_multi_divide(first_round[k]);
         for (let m = 0; m < second_round.length; m++) {
-            [...second_round_backup] = second_round[m]
             let final = plus_minus_multi_divide(second_round[m]);
             for (let n = 0; n < final.length; n++) {
                 if (final[n][0] == Number(target)) {
-                    let result = analysis_back(x, m, n, initial, first_round, second_round, first_round_backup, second_round_backup, inistr, target);
+                    let result = analysis_back(k, m, n, initial, first_round, second_round, target);
                     if (oaArr[0].checked) {
-                        document.getElementById("answer").innerHTML = result
+                        document.getElementById("answer").innerHTML = String(initial) + " can have a result of " + target + "." + result
                         bool_stop = true;
                         break;
                     }
                     else {
                         count = count + 1;
-                        result_all = result_all.concat(result);
+                        result_all.push(result);
                     }
 
                 }
@@ -134,6 +128,6 @@ function calculate_result() {
         }
         if (bool_stop) { break };
     }
-    if (oaArr[0].checked && !bool_stop) { document.getElementById("answer").innerHTML = inistr + " can never have a result of " + target + "." }
-    else if (oaArr[1].checked) { document.getElementById("answer").innerHTML = result_all + String(count) };
+    if (oaArr[0].checked && !bool_stop) { document.getElementById("answer").innerHTML = String(initial) + " can never have a result of " + target + "." }
+    else if (oaArr[1].checked) { document.getElementById("answer").innerHTML = String(initial) + " can have a result of " + target + "."+ result_all + "<br/>"+ String(count) };
 }
