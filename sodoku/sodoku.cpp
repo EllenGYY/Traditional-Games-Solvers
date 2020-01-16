eed591b6c8cc8edd6bedb00cdf93303238d33452
YiyinEllenGu <yiyingu0512@gmail.com>
2020-01-15 21:13:51 -0500

    complete

--- sodoku/sodoku.cpp
+++ sodoku/sodoku.cpp
@@ -1,198 +1,120 @@
-#include <iostream>
 #include <vector>
-#include <map> 
 #include <algorithm>
-#include <iterator>
+#include <iostream>
+#include <map>
+#include <cstdio>
+#include <ctime>
 using namespace std;
 
-int initial_sodoku[9][9] = {
-		{0,7,0,2,0,0,0,8,0},{0,0,0,0,5,0,0,0,0},{0,0,3,6,0,0,0,0,2},
-		{0,0,0,0,0,0,6,0,9},{0,0,0,3,0,0,0,0,0},{3,0,7,0,0,6,0,0,0},
-		{5,0,0,0,0,4,7,0,0},{0,0,0,0,9,0,0,0,0},{0,3,0,0,0,1,0,9,0}
-	};
-map<int,vector<int> > record;
-
-
-void del_impossi(int element, vector<int> *par){
-	vector<int>::iterator it = find((*par).begin(), (*par).end(), element);
-    if (it != (*par).end()) {
-    	(*par).erase(it);
+bool findInFirst(map<int,int> (*record), int element){
+    for(map<int,int>::const_iterator it = (*record).begin(); it != (*record).end(); it ++)
+    {
+        if((it -> first) == element){
+            return true;
+        }
     }
+    return false;
 }
 
-vector<int> find_related(int coor){
-	vector<int> related;
-	int y = coor % 10;
-	int x = (coor - y) / 10;
-	for(int i = 0; i < 9; i ++){
-		if(i != x){
-			related.push_back(10 * i + y);
-		}
-		if(i != y){
-			related.push_back(10 * x + i);
-		}
-	}
-	for (int m = x - x % 3; m < x - x % 3 + 3; m ++){
-        for(int n = y - y % 3; n < y - y % 3 + 3; n ++){
-            if(m != x || n != y){
-            	related.push_back(10 * m + n);
+bool findInSecond(map<int,int> (*record), int element){
+    for(map<int,int>::const_iterator it = (*record).begin(); it != (*record).end(); it ++)
+    {
+        if((*record)[it -> first]== element){
+            return true;
+        }
+    }
+    return false;
+}
+
+void queensFunc(map<int, int> &queens, map<int, int> &grids, vector<vector<int> > *ans, vector<vector<int> > &sodoku, int row){
+    if(queens.size() == 9){
+        vector<int> temp_board;
+        for(int i = 0; i < 9; i ++){
+            temp_board.push_back(queens[i]);
+        }
+        (*ans).push_back(temp_board);
+        return;
+    }
+    map<int, int> gridsn(grids);
+    map<int, int> queensn(queens);
+    if(!findInFirst(&queens, row)){
+        for(int i = 0; i < 9; i ++){
+            if(sodoku[row][i] == 0){
+                int grid = row / 3 * 3 + i / 3;
+                if(findInSecond(&queens, i) || findInSecond(&grids, grid)){
+                    continue;
+                }
+                gridsn[row] = grid;
+                queensn[row] = i;
+                queensFunc(queensn, gridsn, ans, sodoku, ++row);
+                row --;
+            }
+        }
+        return;
+    }
+    row ++;
+    queensFunc(queensn, gridsn, ans, sodoku, row);     
+}
+
+vector<vector<int> > solveNumber(int num, vector<vector<int> > &sodoku) {
+    vector<vector<int> > ans;
+    map<int,int> queens;
+    map<int,int> grids;
+    for(int i = 0; i < 9; i ++){
+        for(int j = 0; j < 9; j ++){
+            if(sodoku[i][j] == num){
+                grids[i] = i / 3 * 3 + j / 3;
+                queens[i] = j;
             }
         }
     }
-	return related;
+    queensFunc(queens, grids, &ans, sodoku, 0);
+    return ans;
 }
 
-void check_before(int coor){
-	vector<int> related = find_related(coor);
-	for(vector<int>::iterator it = related.begin(); it != related.end(); it ++){
-		if(initial_sodoku[((*it) - (*it) % 10) / 10][(*it) % 10] == 0){
-			del_impossi(initial_sodoku[(coor - coor % 10) / 10][coor % 10], &record[*it]);
-			if(record[*it].size() == 1){
-				initial_sodoku[((*it) - (*it) % 10) / 10][(*it) % 10] = record[*it][0];
-				check_before(*it);
-			}
-		}
+void solveWhole(int num, vector<vector<int> > &sodoku, vector<vector<vector<int> > > *whole_ans){
+	if( (*whole_ans).size() > 0){
+		return;
 	}
-}
-
-void possible_choice(int coor){
-	vector<int> possi;
-	vector<int> related = find_related(coor);
-	for(int i = 1; i < 10; i ++){
-		possi.push_back(i);
-	}
-	for(vector<int>::iterator it = related.begin(); it != related.end(); it ++){
-		if(initial_sodoku[((*it) - (*it) % 10) / 10][(*it) % 10] != 0){
-			del_impossi(initial_sodoku[((*it) - (*it) % 10) / 10][(*it) % 10], &possi);
-		}
-	}
-    if(possi.size() == 1){
-    	initial_sodoku[(coor - coor % 10) / 10][coor % 10] = possi[0];
-    	check_before(coor);
+    if(num == 10){
+    	(*whole_ans).push_back(sodoku);
+    	return;
     }
-    else{
-    	record[coor] = possi;
+    vector<vector<int> > possis = solveNumber(num, sodoku);
+    if(possis.size() == 0){
+    	return;
     }
-}
-
-void horizon_check_unique(int coor){
-	int x = (coor - coor % 10) / 10;
-	map<int,vector<int> > record_times;
-	for(int i = 0; i < 9; i ++){
-		if(initial_sodoku[x][i] == 0){
-			for(vector<int>::iterator it = record[10 * x + i].begin(); it != record[10 * x + i].end(); it ++){
-				if(record_times.find(*it) == record_times.end()){
-					vector<int> temp;
-					temp.push_back(10 * x + i);
-					record_times[*it] = temp;
-				}
-				else{
-					record_times[*it].push_back(10 * x + i);
-				}
-			}
-		}
-	}
-	for(map<int,vector<int> >::const_iterator it = record_times.begin(); it != record_times.end(); it ++){
-		if((it -> second).size() == 1){
-			int coor_t = (it -> second)[0];
-			initial_sodoku[(coor_t - coor_t % 10) / 10][coor_t % 10] = (it -> first);
-			check_before(coor_t);
-		}
+    for(vector<vector<int> >::const_iterator it = possis.begin(); it != possis.end(); it ++){
+    	vector<vector<int> > sodokun(sodoku);
+    	for(int i = 0; i < 9; i ++){
+    		sodokun[i][(*it)[i]] = num;
+    	}
+    	solveWhole(++num, sodokun, whole_ans);
+    	num --;
 	}
 }
 
-void vertical_check_unique(int coor){
-	int y = coor % 10;
-	map<int,vector<int> > record_times;
-	for(int i = 0; i < 9; i ++){
-		if(initial_sodoku[i][y] == 0){
-			for(vector<int>::iterator it = record[10 * i + y].begin(); it != record[10 * i + y].end(); it ++){
-				if(record_times.find(*it) == record_times.end()){
-					vector<int> temp;
-					temp.push_back(10 * i + y);
-					record_times[*it] = temp;
-				}
-				else{
-					record_times[*it].push_back(10 * i + y);
-				}
-			}
-		}
-	}
-	for(map<int,vector<int> >::const_iterator it = record_times.begin(); it != record_times.end(); it ++){
-		if((it -> second).size() == 1){
-			int coor_t = (it -> second)[0];
-			initial_sodoku[(coor_t - coor_t % 10) / 10][coor_t % 10] = (it -> first);
-			check_before(coor_t);
-		}
-	}
-}
-
-void grid_check_unique(int coor){
-	int y = coor % 10;
-	int x = (coor - y) / 10;
-	map<int,vector<int> > record_times;
-	for (int m = x - x % 3; m < x - x % 3 + 3; m ++){
-        for(int n = y - y % 3; n < y - y % 3 + 3; n ++){
-            if(initial_sodoku[m][n] == 0){
-            	for(vector<int>::iterator it = record[10 * m + n].begin(); it != record[10 * m + n].end(); it ++){
-					if(record_times.find(*it) == record_times.end()){
-						vector<int> temp;
-						temp.push_back(10 * m + n);
-						record_times[*it] = temp;
-					}
-					else{
-						record_times[*it].push_back(10 * m + n);
-					}
-				}
-            }
-        }
-        for(map<int,vector<int> >::const_iterator it = record_times.begin(); it != record_times.end(); it ++){
-			if((it -> second).size() == 1){
-				int coor_t = (it -> second)[0];
-				initial_sodoku[(coor_t - coor_t % 10) / 10][coor_t % 10] = (it -> first);
-				check_before(coor_t);
-			}
-		}
+int main(){
+	vector<vector<vector<int> > > whole_ans;
+    vector<vector<int> > initial_sodoku;
+    initial_sodoku.resize(9);
+    for(int i = 0; i < 81; i ++){
+    	int a;
+    	cin >> a;
+    	initial_sodoku[i / 9].push_back(a);
     }
-}
-
-void check_unique(int coor){
-	horizon_check_unique(coor);
-	vertical_check_unique(coor);
-	grid_check_unique(coor);
-}
-
-int main()
-{
-	for(int i = 0; i < 9; i ++){
-		for(int j = 0; j < 9; j ++){
-			if(initial_sodoku[i][j] == 0){
-				possible_choice(10 * i + j);
-			}
-		}
-	}
-	for(int i = 0; i <= 88; i += 11){
-		horizon_check_unique(i);
-		vertical_check_unique(i);
-	}
-	for(int i = 0; i <= 60; i += 30){
-		for(int j = 0; j <= 6; j += 3){
-			grid_check_unique(i + j);
-		}
-	}
-	for (int i = 0; i < 9; i ++){
-		for( int j = 0; j < 9; j ++){
-			cout << initial_sodoku[i][j] << ", ";
-		}
-		cout << "\n";
-	}
-	for(map<int,vector<int> >::const_iterator it = record.begin(); it != record.end(); it ++)
-	{
-	    cout << it -> first << " ";
-	    vector<int> temp = it -> second;
-	    for (vector<int>::const_iterator i = temp.begin(); i != temp.end(); i ++){
-    		cout << *i << " ";
-	    }
-	    cout << "\n";
-	}
+    clock_t start = clock();
+    solveWhole(1, initial_sodoku, &whole_ans);
+    double duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
+    cout << duration << endl;
+    for(vector<vector<vector<int> > >::const_iterator ite = whole_ans.begin(); ite != whole_ans.end(); ite ++){
+    	for(vector<vector<int> >::const_iterator it = (*ite).begin(); it != (*ite).end(); it ++){
+	        for (vector<int>::const_iterator i = (*it).begin(); i != (*it).end(); i ++){
+	            cout << *i << " ";
+	        }
+        	cout << "\n";
+    	}
+    	cout << "\n";	
+    }
+    return 0;
 }
